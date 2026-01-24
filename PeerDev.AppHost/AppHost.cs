6e8d1c4a9da914444318f5af4db2128cfa3913b1
sohaibsalman <sohaibsalman10@gmail.com>
2026-01-24 11:59:39 +0500

    Add keyclock identity

--- PeerDev.AppHost/AppHost.cs
+++ PeerDev.AppHost/AppHost.cs
@@ -1,3 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var keyclock = builder.AddKeycloak("keyclock", 6001)
+    .WithDataVolume("keyclock-data"); // To persist keyclock data outside docker container
+
 builder.Build().Run();
