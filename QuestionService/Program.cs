28ebfedefaa7c18d6c44efcd0ace6a0e43e62615
sohaibsalman <sohaibsalman10@gmail.com>
2026-01-24 13:16:20 +0500

    Init questions db

--- QuestionService/Program.cs
+++ QuestionService/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using QuestionService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +15,7 @@ builder.Services.AddAuthentication()
         options.Audience = "peerdev";
     });
 
+builder.AddNpgsqlDbContext<QuestionDbContext>("questionDb");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,5 +29,19 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.MapDefaultEndpoints();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<QuestionDbContext>();
+    await context.Database.MigrateAsync();
+}
+catch (Exception e)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e, "An error occurred seeding the DB.");
+}
 
 app.Run();
