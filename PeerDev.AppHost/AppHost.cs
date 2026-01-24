621afc3657753cf1ebed48a059d36b13deacaa76
sohaibsalman <sohaibsalman10@gmail.com>
2026-01-24 12:57:16 +0500

    Add postgres db service and create db for questions service

--- PeerDev.AppHost/AppHost.cs
+++ PeerDev.AppHost/AppHost.cs
@@ -1,15 +1,24 @@
+#pragma warning disable ASPIRECERTIFICATES001
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-#pragma warning disable ASPIRECERTIFICATES001
 var keycloak = builder.AddKeycloak("keycloak", 6001)
     .WithoutHttpsCertificate()
-#pragma warning restore ASPIRECERTIFICATES001
     .WithDataVolume("keyclock-data"); // To persist keycloak data outside docker container
+
+var postgres = builder.AddPostgres("postgres", port: 5432)
+    .WithDataVolume("postgres-data")
+    .WithPgAdmin();
+
+var questionDb = postgres.AddDatabase("questionDb");
 
 var questionService = builder.AddProject<QuestionService>("question-svc")
     .WithReference(keycloak)
-    .WaitFor(keycloak);
+    .WithReference(questionDb)
+    .WaitFor(keycloak)
+    .WaitFor(questionDb);
 
 builder.Build().Run();
+
+#pragma warning restore ASPIRECERTIFICATES001
