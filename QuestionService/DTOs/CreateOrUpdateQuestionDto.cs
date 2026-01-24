3306dc5cb165504ef00e62285db624e835821fd9
sohaibsalman <sohaibsalman10@gmail.com>
2026-01-24 14:03:25 +0500

    Add CRUD functions for questions service

---
+++ QuestionService/DTOs/CreateOrUpdateQuestionDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+using QuestionService.Validators;
+
+namespace QuestionService.DTOs;
+
+public record CreateOrUpdateQuestionDto(
+    [Required] string Title, 
+    [Required] string Content, 
+    [Required][TagListValidator(1, 5)] List<string> Tags);
